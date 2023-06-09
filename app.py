18449d9d5c81e4a2527fc2093b4f3f1526546685
volchek00 <vaclpech@gmail.com>
2023-06-09 22:22:43 +0200

    runs on GCR

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+from flask import Flask, render_template, request
 import zipfile
 import xml.etree.ElementTree as ET
 import pandas as pd
+import os
 
-def main():
+app = Flask(__name__)
+
+def process_xml_file(zip_file_path):
     items = []
     parts_and_spare_parts = []
 
     # Extracting the file
-    with zipfile.ZipFile("astra_export_xml.zip", "r") as zip_ref:
+    with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
         zip_ref.extractall()
         xml_file = zip_ref.namelist()[0]
 
@@ -35,28 +39,41 @@ def main():
                 item_name = part.attrib.get('itemName')
                 items = part.findall('item')
                 names = [item.attrib.get('name') for item in items]
-                parts_and_spare_parts.append({'Item Name': item_name, 'Names': ', '.join(names)})
+                parts_and_spare_parts.append({'Items': item_name, 'Spare parts': ', '.join(names)})
 
     # Creating a spare_parts_df from the parts_and_spare_parts
     spare_parts_df = pd.DataFrame(parts_and_spare_parts)
 
-    while True:
-        user_input = input("Enter your choice (1, 2, 3, or q to quit): ")
+    return count, all_items_df, spare_parts_df
 
-        if user_input == "1":
-            print(f"Total number of items: {count} items")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-        elif user_input == "2":
-            print(all_items_df['All_items'])
+@app.route('/process', methods=['POST'])
+def process():
+    user_input = request.form['choice']
 
-        elif user_input == "3":
-            print(spare_parts_df)
+    if user_input == "1":
+        count, _, _ = process_xml_file("astra_export_xml.zip")
+        result = f"Total number of items: {count} items"
 
-        elif user_input == "q":
-            break
-        
-        else:
-            print("Invalid input. Please try again.")
+    elif user_input == "2":
+        _, all_items_df, _ = process_xml_file("astra_export_xml.zip")
+        result = all_items_df.to_html(index=False)
+
+    elif user_input == "3":
+        _, _, spare_parts_df = process_xml_file("astra_export_xml.zip")
+        result = spare_parts_df.to_html(index=False)
+
+    elif user_input == "q":
+        result = "Exiting..."
+
+    else:
+        result = "Invalid input. Please try again."
+
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    main()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
